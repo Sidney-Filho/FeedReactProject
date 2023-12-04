313ab86f839314f0a969672ec9a8ae33cf8b5e8a
Sidney Filho <sidinhocostafilho@gmail.com>
2023-12-04 13:18:58 +0000

    first commit on project

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteCommment: (commnet: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    // Sempre que for alterar um valor que depende do valor antigo, utilizar uma função desta maneira abaixo
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/maykbrito.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mayk Brito</strong>
+              <time
+                title="30 de Novembro às 15:57"
+                dateTime="2023-30-11 15:57:00"
+              >
+                {" "}
+                Publicado cerca de 2h atrás{" "}
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
